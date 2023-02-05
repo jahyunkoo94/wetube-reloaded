7723cecc7035a04eae96840b6bb86a004cf1ee3a
jahyunkoo94 <81736745+jahyunkoo94@users.noreply.github.com>
2023-02-05 22:10:29 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,6 +4,33 @@ const PORT = 5000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log("I'm in the middle!");
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("you are allowed");
+  next();
+};
+
+const handleHome = (req, res) => {
+  return res.send("I love middlewares");
+};
+
+const handleProtected = (req, rex) => {
+  return res.send("Welcome ");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
