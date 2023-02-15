f54dafc3a6754181bfaa401389a69c02e1e0e63c
jahyunkoo94 <81736745+jahyunkoo94@users.noreply.github.com>
2023-02-15 21:45:05 +0900

    ~#6.18

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -38,16 +38,18 @@ export const getUpload = (req, res) => {
 
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  await video.create({
-    title,
-    description,
-    createdAt: Date.now(),
-    hasgtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-
-  return res.redirect("/");
+  try {
+    await Video.create({
+      title,
+      description,
+      createdAt: Date.now(),
+      hasgtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
