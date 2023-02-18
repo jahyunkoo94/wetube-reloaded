4e3d548976c80c361d96baa063c4399b8acc1b22
jahyunkoo94 <81736745+jahyunkoo94@users.noreply.github.com>
2023-02-18 16:32:57 +0900

    ~#6.24

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,13 +1,5 @@
 import Video from "../models/Video";
 
-/*
-console.log("start")
-//Video.find({}, (error, videos) => {
-    return res.render("home", { pageTitle: "Home", videos });
-});
-console.log("finished")
-*/
-
 export const home = async (req, res) => {
   try {
     const videos = await Video.find({});
@@ -44,9 +36,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hasntags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -62,7 +52,7 @@ export const postUpload = async (req, res) => {
       title,
       description,
       createdAt: Date.now(),
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
